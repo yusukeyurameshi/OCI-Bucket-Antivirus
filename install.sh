7099cf826585ba06f2ff33e77f8c20f5b1911b17
yusukeyurameshi <fabio@yusuketi.com>
2020-11-07 18:45:58 -0300

    .

--- install.sh
+++ install.sh
@@ -10,16 +10,26 @@ git clone https://github.com/yusukeyurameshi/OCI-Bucket-Antivirus.git
 ## Update ClamAV virus definitions
 #0 10 * * * /usr/bin/freshclam
 
-yum -y install epel-release
-yum clean all
 
-yum -y install clamav clamav-update clamav-data clamav-scanner-systemd clamav-devel
+cat /root/OCI-Bucket-Antivirus/crontab.txt | crontab -
 
-setsebool -P antivirus_can_scan_system 1
-setsebool -P clamd_use_jit 1
-sed -i -e "s/^Example/#Example/" /etc/clamd.d/scan.conf
+
+yum -y install clamav clamav-scanner-systemd
+
+ln -s /etc/clamd.d/scan.conf /etc/clamd.conf
+
 echo "LocalSocket /run/clamd.scan/clamd.sock" >>/etc/clamd.d/scan.conf
+setsebool -P antivirus_can_scan_system 1
 
 freshclam
+
+
+yum -y install clamav-update
+
+
 systemctl start clamd@scan
 systemctl enable clamd@scan
+
+
+
+
