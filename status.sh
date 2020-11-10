d8b8875ab9453ff0819e4bb3f9283cbebda2a996
yusukeyurameshi <fabio@yusuketi.com>
2020-11-10 17:33:16 -0300

    .

--- status.sh
+++ status.sh
@@ -4,7 +4,5 @@ cd /OCI-Bucket-Antivirus
 git pull
 
 cp template.html /var/www/html/index.html
-
-
-$content=`cat /var/log/messages | grep cloud-init`
-sed "s/@@@CONTEUDOINSTALACAO@@@/${content}/g /var/www/html/index.html"
+cat /var/log/messages | grep cloud-init>>/var/www/html/index.html
+echo "</body></html>">>/var/www/html/index.html
